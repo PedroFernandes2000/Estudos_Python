1a85dc773945939bb293397403dae3eaaa4abb16
Pedro Fernandes <130870603+PedroFernandes2000@users.noreply.github.com>
2025-03-14 16:32:02 -0300

    Create conexao.py

---
+++ conexao.py
@@ -0,0 +1,19 @@
+﻿
+import psycopg2
+import time
+
+
+def conexaoComBd():
+    try:               # a porta pode ser colocada separada do host no formato `port=5432`
+        con = psycopg2.connect(database='postgres',user='postgres',host='localhost:5432')
+        cur = con.cursor()                                                          # , passsword='senha'
+        sql = 'SELECT * FROM Source;' # ou mude para o nome da sua tabela
+        cur.execute(sql)
+    except Exception as e:
+        print("Erro:", e)
+  
+
+for i in range(100):
+    conexaoComBd()
+    time.sleep(720) # 12 minutos
+    i = i + 1
